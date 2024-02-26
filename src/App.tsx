017092f590e593158ae0262266fb773ae5c85f1a
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-02-27 00:30:02 +0100

    add basic gh setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import Card from './Card';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Card />
-    </View>
+    <GestureHandlerRootView style={styles.ghRoot}>
+      <View style={styles.container}>
+        <Card />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
+  ghRoot: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: 'rgb(0,0,0)',
--- src/Card.tsx
+++ src/Card.tsx
@@ -11,6 +11,7 @@ import {
   Canvas,
 } from '@shopify/react-native-skia';
 import {Dimensions} from 'react-native';
+import {GestureDetector, Gesture} from 'react-native-gesture-handler';
 
 const WIDTH = Dimensions.get('window').width * 0.9;
 const HEIGHT = 200;
@@ -19,6 +20,8 @@ const PADDING = 4;
 const INNER_BORDER_RADIUS = OUTER_BORDER_RADIUS - PADDING;
 
 export default function Card() {
+  const gesture = Gesture.Pan();
+
   const background = useImage(require('./assets/background.png'));
   const logo = useImage(require('./assets/ck_logo.png'));
 
@@ -37,29 +40,31 @@ export default function Card() {
   const c = center(outerRct);
 
   return (
-    <Canvas style={{width: WIDTH, height: HEIGHT}}>
-      <RoundedRect rect={outerRct}>
-        <SweepGradient c={c} colors={['cyan', 'magenta', 'cyan']} />
-      </RoundedRect>
-      <RoundedRect rect={innerRct} />
-      <Group clip={innerRct}>
-        <Image
-          image={background}
-          fit="contain"
-          x={75}
-          y={25}
-          width={WIDTH}
-          height={HEIGHT}
-        />
-        <Image
-          image={logo}
-          fit="contain"
-          x={15}
-          y={15}
-          width={100}
-          height={20}
-        />
-      </Group>
-    </Canvas>
+    <GestureDetector gesture={gesture}>
+      <Canvas style={{width: WIDTH, height: HEIGHT}}>
+        <RoundedRect rect={outerRct}>
+          <SweepGradient c={c} colors={['cyan', 'magenta', 'cyan']} />
+        </RoundedRect>
+        <RoundedRect rect={innerRct} />
+        <Group clip={innerRct}>
+          <Image
+            image={background}
+            fit="contain"
+            x={75}
+            y={25}
+            width={WIDTH}
+            height={HEIGHT}
+          />
+          <Image
+            image={logo}
+            fit="contain"
+            x={15}
+            y={15}
+            width={100}
+            height={20}
+          />
+        </Group>
+      </Canvas>
+    </GestureDetector>
   );
 }
